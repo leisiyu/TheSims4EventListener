6b2a8d77a7ae788bbdafa382ff6896fd26a7ade5
leisiyu <leisiyu320@gmail.com>
2025-05-11 23:07:26 +0100

    add: get location command add: get zone id command

--- Sims 4 Python Script Workspace/My Script Mods/Example Mod/Scripts/example_mod.py
+++ Sims 4 Python Script Workspace/My Script Mods/Example Mod/Scripts/example_mod.py
@@ -2,21 +2,27 @@ import sims4.commands
 import services
 import json
 
+from my_mod_events import MyModEvents
+
 # from server_commands.zone_commands import register_zone_loaded_callback
-from event_testing.test_events import TestEvent
+
 
 @sims4.commands.Command('myfirstscript', command_type=sims4.commands.CommandType.Live)
 def myfirstscript(_connection=None):
     output = sims4.commands.CheatOutput(_connection)
     output("this is my first script")
     with open('D:\Qianwen\Mods\example.txt', 'a') as file:
-        file.write('This is my first script mod')#
+        file.write('This is my first script mod \n')#
 
-@sims4.commands.Command('simInfo', command_type=sims4.commands.CommandType.Live)
+@sims4.commands.Command('siminfo', command_type=sims4.commands.CommandType.Live)
 def simInfo(_connection=None):
-    sim_info = services.active_sim_info()
+    # sim_info = services.active_sim_info()
+    client = services.client_manager().get_first_client()
+    sim_info = client.active_sim.sim_info
+
     output = sims4.commands.CheatOutput(_connection)
-    output(sim_info+ "\n")
+    output(sim_info + "\n")
+
 
 
 @sims4.commands.Command('getname', command_type=sims4.commands.CommandType.Live)
@@ -27,7 +33,32 @@ def getSimName(_connection=None):
     with open('D:\Qianwen\Mods\example.txt', 'a') as file:
         file.write('full name is ' + full_name + "\n")#
     output = sims4.commands.CheatOutput(_connection)
+
     output("name: " + full_name)
+
+@sims4.commands.Command('getlocation', command_type=sims4.commands.CommandType.Live)
+def getSimLocation(_connection=None):
+    client = services.client_manager().get_first_client()
+    sim_info = client.active_sim.sim_info
+    position = sim_info.get_sim_instance().transform.translation
+    position_str = f"X: {position.x}, Y: {position.y}, Z: {position.z}"
+    output = sims4.commands.CheatOutput(_connection)
+    output("location:" + position_str)
+    with open('D:\Qianwen\Mods\example.txt', 'a') as file:
+        file.write('location is: ' + position_str + "\n")  #
+
+
+@sims4.commands.Command('getzone', command_type=sims4.commands.CommandType.Live)
+def getSimZone(_connection=None):
+    output = sims4.commands.CheatOutput(_connection)
+    current_zone = services.current_zone()
+    if current_zone is not None:
+        zone_id = current_zone.id  # Get the current zone ID
+        output(f"Sim is in zone: {zone_id}")
+        with open('D:\\Qianwen\\Mods\\example.txt', 'a') as file:
+            file.write(f"Sim is in zone: {zone_id}\n")
+    else:
+        output("Unable to retrieve the current zone.")
 
 @sims4.commands.Command('getingametime', command_type=sims4.commands.CommandType.Live)
 def getInGameTime(_connection=None):
@@ -50,12 +81,7 @@ def getInGameTime(_connection=None):
         file.write(results + "\n")
 
 
-# def on_zone_loaded(zone_id):
-#     output = sims4.commands.CheatOutput(_connection)
-#     output("zone: " + zone_id)
-#     with open('D:\Qianwen\Mods\example.txt', 'a') as file:
-#         file.write('zone id is ' + zone_id + "\n")
-#
-# @sims4.commands.Command('zone', command_type=sims4.commands.CommandType.Live)
-# def registerZone(zone_id=None, _connection=None):
-#     sims4.callback_utils.register_zone_loaded_callback(on_zone_loaded)
+@sims4.commands.Command('regevent', command_type=sims4.commands.CommandType.Live)
+def regEvent(_connection=None):
+    myModEvents = MyModEvents("test")
+    myModEvents.registerEvent()
