2d1ff156beb476864e5fbbb55e31b00d45ab8e89
leisiyu <leisiyu320@gmail.com>
2025-05-19 00:33:13 +0100

    add: event json

--- Sims 4 Python Script Workspace/My Script Mods/Example Mod/Scripts/my_mod_events.py
+++ Sims 4 Python Script Workspace/My Script Mods/Example Mod/Scripts/my_mod_events.py
@@ -3,6 +3,7 @@ import services
 from event_testing.test_events import TestEvent
 from interactions import ParticipantType
 import Util
+import json
 
 
 class MyModEvents():
@@ -17,14 +18,22 @@ class MyModEvents():
             return
         # participant = resolver.get_participant(ParticipantType.Actor)
 
-        # timeline_now = services.time_service().sim_now
-        # time_str = timeline_now
+        timeline_now = services.time_service().sim_now
+        time_str = timeline_now
         interaction = resolver.interaction  # Access the completed interaction
         target = interaction.target  # Get the target of the interaction
         interaction_name = interaction.affordance.__name__  # Name of the interaction
+        simName = sim_info.first_name + " " + sim_info.last_name
+
+        eventJson = {
+            "sim_name": simName,
+            "interaction_name": interaction_name,
+            "target": str(target),
+            "time": str(time_str)
+        }
 
         with open(logDir, 'a') as file:
-            file.write(f"Sim: {sim_info.first_name}, Interaction: {interaction_name}, Target: {target}\n")
+            file.write(json.dumps(eventJson) + "\n")
 
 
     def registerEvent(self):
