d7d8cb399f74a902f3b5a21150784e18dbd6d773
leisiyu <leisiyu320@gmail.com>
2025-06-13 00:00:24 +0100

    update: mod file update: log file

--- Sims 4 Python Script Workspace/My Script Mods/Example Mod/Scripts/my_mod_events.py
+++ Sims 4 Python Script Workspace/My Script Mods/Example Mod/Scripts/my_mod_events.py
@@ -25,6 +25,7 @@ class MyModEvents():
         interaction_name = interaction.affordance.__name__  # Name of the interaction
         simName = sim_info.first_name + " " + sim_info.last_name
 
+
         actor = resolver.get_participant(ParticipantType.Actor)
         target = resolver.get_participant(ParticipantType.TargetSim)
 
@@ -35,23 +36,18 @@ class MyModEvents():
         current_zone = services.current_zone()
         if current_zone is not None:
             zone_id = current_zone.id  # Get the current zone ID
-            zone_name = current_zone.lot.get_lot_name()
+            building_name = current_zone.lot.get_lot_name()
 
-        position = sim_info.get_sim_instance().transform.translation
-        position_str = f"X: {position.x} Y: {position.y} Z: {position.z}"
 
-        # plex_service = services.get_plex_service()
-        # if plex_service is not None:
-        #     building = plex_service.get_building_by_zone_id(services.current_zone_id())
+        # position = sim_info.get_sim_instance().transform.translation
+        # position_str = f"X: {position.x} Y: {position.y} Z: {position.z}"
 
 
         eventJson = {
             "sim_name": simName,
             "target_name": target_name,
             "interaction_name": interaction_name,
-            "zone": zone_name,
-            # "building": building,
-            "sim_position": position_str,
+            "building_name": building_name,
             "time": str(time_str)
         }
 
