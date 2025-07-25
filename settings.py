e6eefb9c471ba5fc83da4d7408f6ce8cca6b69db
leisiyu <leisiyu320@gmail.com>
2025-07-25 23:47:56 +0100

    amend: path name

--- settings.py
+++ settings.py
@@ -2,7 +2,7 @@ import os
 
 creator_name = 'QL'
 mods_folder = os.path.expanduser(
-    os.path.join('D:', os.sep, 'Qianwen', 'Mods')
+    os.path.join('D:', os.sep, 'QL', 'Mods')
 )
 
 game_folder = os.path.join('C:', os.sep, 'Program Files (x86)', 'Steam', 'steamapps', 'common', 'The Sims 4')
